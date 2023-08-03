15da1f009d130b9232065c3a1f165db106c38b12
맥찡 <rkdeorb0124@naver.com>
2023-08-03 21:12:31 +0900

    commit source

---
+++ lib/home_tap.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:fluttermainproject/view/apartment_view.dart';
+import 'package:fluttermainproject/view/user_view.dart';
+import 'package:fluttermainproject/view/wishlist_view.dart';
+import 'package:get/get.dart';
+
+class HomeTap extends StatefulWidget{
+  const HomeTap({super.key});
+
+  @override
+  State<HomeTap> createState() => _HomeTapState();
+}
+
+class _HomeTapState extends State<HomeTap> with SingleTickerProviderStateMixin {
+
+  // Property
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this, initialIndex: 0);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    int initialTabIndex = Get.arguments ?? 0;
+    _tabController.index = initialTabIndex;
+    return Scaffold(
+      body: TabBarView(
+        controller: _tabController,
+        children: const [
+          ApartmentView(),
+          WishlistView(),
+          UserView()
+        ],
+      ),
+      bottomNavigationBar: TabBar(
+        labelColor: Colors.green,
+        controller: _tabController,
+        tabs: const [
+          Tab(
+            icon: Icon(
+              Icons.pin_drop,
+            ),
+            text: 'Map',
+          ),
+          Tab(
+            icon: Icon(
+              Icons.star,
+            ),
+            text: 'WishList',
+          ),
+          Tab(
+            icon: Icon(
+              Icons.add_circle,
+            ),
+            text: '더보기',
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/view/user_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class UserView extends StatelessWidget {
+  const UserView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('user'),
+      ),
+    );
+  }
+}
